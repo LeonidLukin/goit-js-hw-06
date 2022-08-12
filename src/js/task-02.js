fac4eb99cb3e087fa13da16077bb84d9a8e29aab
LeonidLukin <leonidlukin90@gmail.com>
2022-08-12 17:04:00 +0300

    Task № 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listRef = document.getElementById('ingredients')
+
+const makeItem = ingredients => {
+  const item = document.createElement('li')
+  item.classList.add('item')
+  item.textContent = ingredients
+
+  listRef.append(item)
+  return item
+}
+
+const elements = ingredients.map(makeItem)
+
+listRef.append(...elements)
