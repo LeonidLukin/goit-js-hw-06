1baf1e753412a26c589daefead9a8542c8452ed3
LeonidLukin <leonidlukin90@gmail.com>
2022-08-14 11:38:53 +0300

    Task №8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form')
+const inputs = form.querySelectorAll('input')
 
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+    event.preventDefault()
+
+    for (const input of inputs) {
+        if (input.value === '') {
+            return alert('Все поля должны быть заполнены!')
+        }
+    }
+
+    const formElements = event.currentTarget.elements
+    const mail = formElements.email.value
+    const password = formElements.password.value
+
+    const formData = {
+        mail,
+        password,
+    }
+    console.log(formData);
+    form.reset()
+}
