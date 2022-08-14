09a03041bab4684cb7c1fa53f96787a4c77b4eb6
LeonidLukin <leonidlukin90@gmail.com>
2022-08-14 12:26:08 +0300

    Task №9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
+const spanRef = document.querySelector('.color')
+const changeColor = document.querySelector('.change-color')
+
+changeColor.addEventListener('click', getRandomHexColor)
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const newColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+  document.body.style.background = newColor
+  spanRef.textContent = newColor
 }
